8bf3d5ff17a3e271c475f718cc913892f316b86d
chmo-bu <98070551+chmo-bu@users.noreply.github.com>
2022-04-28 12:54:31 -0400

    Update io.py

--- StutterNet/io.py
+++ StutterNet/io.py
@@ -6,54 +6,42 @@ import librosa
 class SEP28KDataset(torch.utils.data.Dataset):
     """SEP-28k Dataset."""
 
-    def __init__(self, csv_file, root_dir, transform=None):
+    def __init__(self, x, y, unsqueeze=False, transform=None):
         """
         Args:
-            csv_file (string): Path to the csv file with annotations.
-            root_dir (string): Directory with all the images.
-            transform (callable, optional): Optional transform to be applied
+            x (hdf5): hdf5 data one of 'Xtrain', 'Xtest', or 'Xvalid'
+            y (hdf5): hdf5 file one of 'Ytrain', 'Ytest', or 'Yvalid'
+            unsqueeze (bool, Optional): Whether or not to unsqueeze the feature.
+              May be required for models that require image-like inputs.
+            transform (callable, Optional): Optional transform to be applied
                 on a sample.
         """
-        annotations = pd.read_csv(csv_file)
-        self.info = annotations.iloc[:, :3].copy()
-        self.labels = annotations[["PoorAudioQuality", "Unsure",
-          "DifficultToUnderstand", "NaturalPause","Music","NoSpeech",
-          "Prolongation","Block","SoundRep","WordRep","Interjection",
-          "NoStutteredWords"]].copy()
-        # [0.11, 0.25, 0.40, 0.62, 0.57, 0.39]
-        # self.spec = audio.transforms.MelSpectrogram(n_mels=40, sample_rate=16000,
-        #                                        n_fft=512, pad=1, f_max=8000, f_min=0,
-        #                                        power=0.5, hop_length=160, win_length=480)
+        self.data = x
+        self.labels = y
         self.spec = audio.transforms.MelSpectrogram(n_mels=80, sample_rate=16000,
                                                n_fft=512, f_max=8000, f_min=0,
                                                power=0.5, hop_length=152, win_length=480)
         self.db = audio.transforms.AmplitudeToDB()
 
         self.freq_mask = audio.transforms.FrequencyMasking(freq_mask_param=1)
-        # self.time_mask = audio.transforms.TimeMasking(freq_mask_param=20)
+        # self.time_mask = audio.transforms.TimeMasking(time_mask_param=20)
 
         self.rng = np.random.default_rng(42)
         # self.rng_2 = np.random.default_rng(68)
+        self.unsqueeze = unsqueeze
 
-        self.root_dir = root_dir
         self.transform = transform
 
     def __len__(self):
-        return len(self.info)
+        return len(self.data)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        # get path attributes and clip interval
-        row = self.info.iloc[idx, :]
-        
-        # get clip path
-        clip_path = f"{self.root_dir}/{row[0]}/{row[1]}/{row[0]}_{row[1]}_{row[2]}.wav"
-
         # load sliced clip
         # _, wav = wavfile.read(clip_path)
-        wav, _ = librosa.load(clip_path, 16000)
+        wav = self.data[idx]
         wav = self.pad_trunc(wav, 3000, 16000).astype('float32')
 
         wav = torch.tensor(wav)
@@ -66,21 +54,11 @@ class SEP28KDataset(torch.utils.data.Dataset):
         # if (self.rng_2.choice(2,p=[0.2,0.8])):
         #   wav = self.time_mask(wav)
 
-        # wav = torch.unsqueeze(wav, 0)
+        if (self.unsqueeze):
+          wav = torch.unsqueeze(wav, 0)
 
         # get labels
-        labels = self.labels.iloc[idx, :].values
-        labels = (labels >= 2).astype('float32')
-
-        # ndl = np.array([ndl])
-        # ndl = ndl.astype('float').reshape(-1, 1)
-
-        # sl = np.array([sl])
-        # sl = sl.astype('float').reshape(-1, 1)
-
-        # labels = np.array([labels])
-        # labels = labels.astype('float').reshape(-1, 1)
-        # sample = {'clip': wav, 'stuttering': sl, 'non_stuttering': ndl}
+        labels = self.labels[idx].astype('float32')
 
         # if self.transform:
         #     sample = self.transform(sample)
