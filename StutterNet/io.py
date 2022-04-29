5b752e27777dcb03472a7d057b55e9a4acd05732
chmo-bu <98070551+chmo-bu@users.noreply.github.com>
2022-04-28 20:02:59 -0400

    Update io.py

--- StutterNet/io.py
+++ StutterNet/io.py
@@ -59,7 +59,7 @@ class SEP28KDataset(torch.utils.data.Dataset):
         labels = self.labels[idx].astype('float32')
 
         if self.transform is not None:
-            wav = self.transform(sample)
+            wav = self.transform(wav)
         
         if (self.unsqueeze):
             wav = torch.unsqueeze(wav, 0)
